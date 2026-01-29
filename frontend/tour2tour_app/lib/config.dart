1c83c2c0c0fdc0b7d4fef3999a859bdae868ca35
Frolova Valeria <vafrolova_3@edu.hse.ru>
2026-01-30 01:02:16 +0300

    New domen

--- frontend/tour2tour_app/lib/config.dart
+++ frontend/tour2tour_app/lib/config.dart
@@ -2,8 +2,7 @@
 import 'package:flutter/foundation.dart';
 
 class Config {
-  static const _androidEmulator = 'http://10.0.2.2:8000';
-  static const _web = 'http://127.0.0.1:8000';
+  static const _api = 'https://api.24tour2tour.ru';
 
-  static String get apiBaseUrl => kIsWeb ? _web : _androidEmulator;
+  static String get apiBaseUrl => _api;
 }
