23166ee9509f68a0260f9dbfdfb51750ba8c27ec
Frolova Valeria <vafrolova_3@edu.hse.ru>
2026-01-30 02:26:11 +0300

    add websites

--- services/trips-service/app/main.py
+++ services/trips-service/app/main.py
@@ -16,6 +16,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:8000",
+        "https://24tour2tour.ru",
+        "https://api.24tour2tour.ru",
     ],
     allow_credentials=True,
     allow_methods=["*"],
