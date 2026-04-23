ab8c5cdeb822149ebb9747196ddf49e72e72bbfa
Frolova Valeria <vafrolova_3@edu.hse.ru>
2026-04-23 17:18:13 +0300

    migration

---
+++ services/trips-service/alembic/versions/0004_merge_destination_city_and_stages.py
@@ -0,0 +1,19 @@
+"""merge destination city and stages branches
+
+Revision ID: 0004_merge_destination_city_and_stages
+Revises: 0002_add_destination_city, 0003_stages
+Create Date: 2026-04-23 00:00:00.000000
+"""
+
+revision = "0004_merge_destination_city_and_stages"
+down_revision = ("0002_add_destination_city", "0003_stages")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
